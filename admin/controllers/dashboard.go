3a17f62fa676d31a786e0361ced6c182c75c93c8
cumhurmessuttokalak <79723267+cumhurmessuttokalak@users.noreply.github.com>
2022-03-21 22:58:11 +0300

    Commit

--- admin/controllers/dashboard.go
+++ admin/controllers/dashboard.go
@@ -1,6 +1,7 @@
 package controllers
 
 import (
+	"Go-Blog/admin/helpers"
 	"fmt"
 	"github.com/julienschmidt/httprouter"
 	"html/template"
@@ -11,10 +12,10 @@ type Dashboard struct {
 }
 
 func (dashboard Dashboard) Index(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
-	view, err := template.ParseFiles("admin/views/dashboard/list/index.html")
+	view, err := template.ParseFiles(helpers.Include("dashboard/list")...)
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
-	view.Execute(w, nil)
+	view.ExecuteTemplate(w, "index", nil)
 }
