efd21bb06275cd2476ab4e0b015bd43c778248a6
cumhurmessuttokalak <79723267+cumhurmessuttokalak@users.noreply.github.com>
2022-03-22 19:31:13 +0300

    Models created

--- main.go
+++ main.go
@@ -1,10 +1,14 @@
 package main
 
 import (
+	admin_models "Go-Blog/admin/models"
 	"Go-Blog/config"
 	"net/http"
 )
 
 func main() {
+	admin_models.Post{}.Migrate()
+	post := admin_models.Post{}.Get(5)
+	post.Delete()
 	http.ListenAndServe(":8080", config.Routes())
 }
