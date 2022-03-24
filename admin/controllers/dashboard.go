3db35f0d085c3874c424e38bde8d912d1cfe357b
cumhurmessuttokalak <79723267+cumhurmessuttokalak@users.noreply.github.com>
2022-03-24 13:58:39 +0300

    Edit and delete operations

--- admin/controllers/dashboard.go
+++ admin/controllers/dashboard.go
@@ -2,10 +2,15 @@ package controllers
 
 import (
 	"Go-Blog/admin/helpers"
+	"Go-Blog/admin/models"
 	"fmt"
+	"github.com/gosimple/slug"
 	"github.com/julienschmidt/httprouter"
 	"html/template"
+	"io"
 	"net/http"
+	"os"
+	"strconv"
 )
 
 type Dashboard struct {
@@ -17,7 +22,9 @@ func (dashboard Dashboard) Index(w http.ResponseWriter, r *http.Request, params 
 		fmt.Println(err)
 		return
 	}
-	view.ExecuteTemplate(w, "index", nil)
+	data := make(map[string]interface{})
+	data["Posts"] = models.Post{}.GetAll()
+	view.ExecuteTemplate(w, "index", data)
 }
 func (dashboard Dashboard) NewItem(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
 	view, err := template.ParseFiles(helpers.Include("dashboard/add")...)
@@ -26,4 +33,55 @@ func (dashboard Dashboard) NewItem(w http.ResponseWriter, r *http.Request, param
 		return
 	}
 	view.ExecuteTemplate(w, "index", nil)
+}
+func (dashboard Dashboard) Add(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
+	title := r.FormValue("blog-title")
+	slug := slug.Make(title)
+	description := r.FormValue("blog-desc")
+	categoryID, _ := strconv.Atoi(r.FormValue("blog-category"))
+	content := r.FormValue("blog-content")
+
+	//upload
+	r.ParseMultipartForm(10 << 20)
+	file, header, err := r.FormFile("blog-picture")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	f, err := os.OpenFile("uploads/"+header.Filename, os.O_WRONLY|os.O_CREATE, 0666)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	_, err = io.Copy(f, file)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	//End upload
+	models.Post{
+		Title:       title,
+		Slug:        slug,
+		Description: description,
+		CategoryID:  categoryID,
+		Content:     content,
+		Picture_url: "uploads/" + header.Filename,
+	}.Add()
+	http.Redirect(w, r, "/admin", http.StatusSeeOther)
+	//ToDo ALERT MESSAGE
+}
+func (dashboard Dashboard) Delete(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
+	post := models.Post{}.Get(params.ByName("id"))
+	post.Delete()
+	http.Redirect(w, r, "/admin", http.StatusSeeOther)
+}
+func (dashboard Dashboard) Edit(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
+	view, err := template.ParseFiles(helpers.Include("dashboard/edit")...)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	data := make(map[string]interface{})
+	data["Post"] = models.Post{}.Get(params.ByName("id"))
+	view.ExecuteTemplate(w, "index", data)
 }
